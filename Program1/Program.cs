437eebfbcb35f12aa0e8dea617cf1fa4a97a15a6
86180 <86180@LAPTOP-PH4RH3QG>
2018-09-11 23:28:16 +0800

    1

---
+++ Program1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.Write("a = ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = double.Parse(Console.ReadLine());
+            c = a * b;
+            Console.WriteLine("a * b = {0}", c);
+        }
+    }
+}
