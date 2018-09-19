bd4eaf602e3341d71eaacb6222ccf2fd8136ed9a
86180 <86180@LAPTOP-PH4RH3QG>
2018-09-19 16:49:29 +0800

    2

--- homework1/Program1/Program.cs
+++ homework1/Program1/Program.cs
@@ -10,13 +10,27 @@ namespace Program1
     {
         static void Main(string[] args)
         {
-            double a, b, c;
-            Console.Write("a = ");
-            a = double.Parse(Console.ReadLine());
-            Console.Write("b = ");
-            b = double.Parse(Console.ReadLine());
-            c = a * b;
-            Console.WriteLine("a * b = {0}", c);
+            Console.WriteLine("please input some numbers,end with 0");
+            int[] a = new int[100];
+            int x = Convert.ToInt32(Console.Read()),len = 0;
+            while (x != 0)
+            {
+                a[len++] = x;
+                x = Convert.ToInt32(Console.Read());
+            }
+            int maxn = a[0], mixn = a[0], sum = 0;
+            for(int i = 0; i < len; i++)
+            {
+                sum += a[i];
+                maxn = Math.Max(maxn, a[i]);
+                mixn = Math.Min(mixn, a[i]);
+
+            }
+            //a = double.Parse(Console.ReadLine());
+            //Console.Write("b = ");
+            //b = double.Parse(Console.ReadLine());
+            //c = a * b;
+            //Console.WriteLine("a * b = {0}", c);
         }
     }
 }
